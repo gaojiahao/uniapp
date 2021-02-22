86e9b155567c7d837f502c1c00d48e92eb2a80f8
jijiliyugou <597892949@qq.com>
2021-02-22 09:33:14 +0800

    更新项目目录

---
+++ toysbear-pc/src/router/dontLoad/index.js
@@ -0,0 +1,39 @@
+const DontLoadDerivedSampleList = () => import('@/components/dontLoad/dontLoadDerivedSampleList/dontLoadDerivedSampleList.vue')
+const DontLoadShareFactory = () => import('@/components/dontLoad/dontLoadShareFactory/dontLoadShareFactory.vue')
+const DontLoadShareFactoryAllProducts = () => import('@/components/dontLoad/dontLoadShareFactory/dontLoadShareFactoryAllProducts/dontLoadShareFactoryAllProducts.vue')
+const DontLoadShareFactoryProductDetails = () => import('@/components/dontLoad/dontLoadShareFactory/dontLoadShareFactoryProductDetails/dontLoadShareFactoryProductDetails.vue')
+const DontLoadShareFactoryPC = () => import('@/components/dontLoad/dontLoadShareFactory/dontLoadShareFactoryPC/dontLoadShareFactoryPC.vue')
+const DontLoadShareFactoryAllProductsPC = () => import('@/components/dontLoad/dontLoadShareFactory/dontLoadShareFactoryPC/dontLoadShareFactoryAllProductsPC/dontLoadShareFactoryAllProductsPC.vue')
+
+export default [
+  {
+    path: '/dontLoadDerivedSampleList',
+    name: 'dontLoadDerivedSampleList',
+    component: DontLoadDerivedSampleList
+  },
+  {
+    path: '/dontLoadShareFactory',
+    name: 'dontLoadShareFactory',
+    component: DontLoadShareFactory
+  },
+  {
+    path: '/dontLoadAllProducts',
+    name: 'dontLoadAllProducts',
+    component: DontLoadShareFactoryAllProducts
+  },
+  {
+    path: '/dontLoadProductDetails',
+    name: 'dontLoadProductDetails',
+    component: DontLoadShareFactoryProductDetails
+  },
+  {
+    path: '/dontLoadShareFactoryPC',
+    name: 'dontLoadShareFactoryPC',
+    component: DontLoadShareFactoryPC
+  },
+  {
+    path: '/dontLoadShareFactoryAllProductsPC',
+    name: 'dontLoadShareFactoryAllProductsPC',
+    component: DontLoadShareFactoryAllProductsPC
+  }
+]
