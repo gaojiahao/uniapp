a0b61fbd9f061a14886f5965fefd22f51b74c02d
chengMin <656330037>
2021-04-07 18:05:22 +0800

    报价接口联通

--- toys-bear-pc/src/components/bsComponents/bsSampleComponent/bsSampleQuotationDetails.vue
+++ toys-bear-pc/src/components/bsComponents/bsSampleComponent/bsSampleQuotationDetails.vue
@@ -5,23 +5,23 @@
       <div class="handerTop">
         <div class="flex_t">
           <p>
-            报价单号：<span class="pId">{{ item.offerNumber }} </span>
+            报价单号：<span class="pId">{{ itemList.offerNumber }} </span>
           </p>
           <p>
-            客户名称：<span>{{ item.customerName }} </span>
+            客户名称：<span>{{ itemList.customerName }} </span>
           </p>
           <p>
-            操作员：<span style="color:#2D7FE4">{{ item.linkman }} </span>
+            操作员：<span style="color:#2D7FE4">{{ itemList.linkman }} </span>
           </p>
           <p>
             状态：<span
               :style="{
-                color: item.status == 0 ? '#3368A9' : '#2D7FE4'
+                color: itemList.status == 0 ? '#3368A9' : '#2D7FE4'
               }"
               >{{
-                item.status == 0
+                itemList.status == 0
                   ? "未审核"
-                  : item.status == 1
+                  : itemList.status == 1
                   ? "审核通过"
                   : "审核不通过"
               }}
@@ -30,9 +30,11 @@
         </div>
         <div class="flex_b">
           <p>
-            报价时间：<span>{{ item.createdOn }} </span>
+            报价时间：<span>{{ itemList.createdOn }} </span>
           </p>
-          <p>报价备注：<span>{{}} </span></p>
+          <p>
+            报价备注：<span>{{ itemList.title }} </span>
+          </p>
         </div>
       </div>
       <div class="handerTab">
@@ -44,22 +46,50 @@
           border
           lazy
         >
-          <el-table-column prop="date" label="默认公式" width="180">
+          <el-table-column
+            align="center"
+            prop="offerMethod"
+            label="报价方式"
+            width="180"
+          >
           </el-table-column>
-          <el-table-column prop="name" label="报价方式" width="180">
+          <el-table-column align="center" prop="profit" label="利润率">
           </el-table-column>
-          <el-table-column prop="address" label="利润率"> </el-table-column>
-          <el-table-column prop="name" label="总费用" width="180">
+          <el-table-column
+            align="center"
+            prop="totalCost"
+            label="总费用"
+            width="180"
+          >
           </el-table-column>
-          <el-table-column prop="name" label="汇率" width="180">
+          <el-table-column
+            align="center"
+            prop="exchange"
+            label="汇率"
+            width="180"
+          >
           </el-table-column>
-          <el-table-column prop="address" label="小数位数"> </el-table-column>
+          <el-table-column align="center" prop="decimalPlaces" label="小数位数">
+          </el-table-column>
 
-          <el-table-column prop="address" label="币种"> </el-table-column>
-          <el-table-column prop="address" label="每车尺寸"> </el-table-column>
-          <el-table-column prop="address" label="取舍方式"> </el-table-column>
-          <el-table-column prop="address" label="价钱小于"> </el-table-column>
-          <el-table-column prop="address" label="小数位数"> </el-table-column>
+          <el-table-column align="center" prop="cu_deName" label="币种">
+          </el-table-column>
+          <el-table-column align="center" prop="size" label="每车尺寸">
+          </el-table-column>
+          <el-table-column
+            align="center"
+            prop="rejectionMethod"
+            label="取舍方式"
+          >
+          </el-table-column>
+          <el-table-column align="center" prop="miniPrice" label="价钱小于">
+          </el-table-column>
+          <el-table-column
+            align="center"
+            prop="miniPriceDecimalPlaces"
+            label="小数位数"
+          >
+          </el-table-column>
         </el-table>
       </div>
     </div>
@@ -304,15 +334,30 @@ export default {
       tableData: [],
       currentPage: 1,
       pageSize: 10,
-      totalCount: 0
+      totalCount: 0,
+      itemList: {}
     };
   },
   created() {},
 
   mounted() {
     this.getProductOfferDetailPage();
+    this.getProductOfferNumber();
   },
   methods: {
+    // 获取该订单报价公式详情
+    async getProductOfferNumber() {
+      const fd = {
+        OfferNumber: this.item.offerNumber
+      };
+      const res = await this.$http.post("/api/GetProductOfferByNumber", fd);
+      if (res.data.result.code === 200) {
+        this.handerTabData.push(res.data.result.item);
+        this.itemList = res.data.result.item;
+      } else {
+        this.$message.error(res.data.result.msg);
+      }
+    },
     // 获取列表
     async getProductOfferDetailPage() {
       const res = await this.$http.post(
