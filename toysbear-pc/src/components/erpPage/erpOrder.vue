17db19bc942b88e304c62054c5c639d037ba15a7
jijiliyugou <597892949@qq.com>
2021-04-15 20:25:41 +0800

    修改erpOrder字段显示

--- toysbear-pc/src/components/erpPage/erpOrder.vue
+++ toysbear-pc/src/components/erpPage/erpOrder.vue
@@ -241,13 +241,20 @@
               align="center"
             >
               <template slot-scope="scope">
-                <div class="remarkClass">
-                  {{
-                    scope.row.remark && scope.row.orderType != "Sample"
-                      ? scope.row.remark
-                      : ""
-                  }}
-                </div>
+                <el-tooltip
+                  class="item"
+                  effect="dark"
+                  :content="scope.row.remark"
+                  placement="top"
+                >
+                  <div class="remarkClass">
+                    {{
+                      scope.row.remark && scope.row.orderType != "Sample"
+                        ? scope.row.remark
+                        : ""
+                    }}
+                  </div>
+                </el-tooltip>
               </template>
             </el-table-column>
             <el-table-column
