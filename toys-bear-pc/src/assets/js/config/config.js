269510f089b746973ecffc627454db70b3b7918f
jijiliyugou <597892949@qq.com>
2021-04-20 09:50:10 +0800

    完成打包加hash值清除缓存

--- toys-bear-pc/src/assets/js/config/config.js
+++ toys-bear-pc/src/assets/js/config/config.js
@@ -17,12 +17,14 @@ module.exports = {
     // wsBaseUrl: "wss://impush.toysbear.com/ws?UserId="
   },
   proEnv: {
-    NODE_ENV: "production", // 生产环境
-    hosturl: "http://139.9.71.135:8080",
-    loginUrl: "http://139.9.71.135:8080/#/me",
-    wsBaseUrl: "ws://139.9.71.135:8090/ws?UserId="
-    // hosturl: "https://www.toysbear.com",
-    // loginUrl: "https://www.toysbear.com/#/me",
-    // wsBaseUrl: "wss://impush.toysbear.com/ws?UserId="
+    NODE_ENV: "production",
+    // 测试环境
+    // hosturl: "http://139.9.71.135:8080",
+    // loginUrl: "http://139.9.71.135:8080/#/me",
+    // wsBaseUrl: "ws://139.9.71.135:8090/ws?UserId="
+    // 生产环境
+    hosturl: "https://www.toysbear.com",
+    loginUrl: "https://www.toysbear.com/#/me",
+    wsBaseUrl: "wss://impush.toysbear.com/ws?UserId="
   }
 };
