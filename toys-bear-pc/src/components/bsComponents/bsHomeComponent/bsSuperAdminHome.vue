656f5848e5977aa997a028f92bec65549cfa4525
chengMin <656330037>
2021-05-13 19:13:58 +0800

    安装echarts

---
+++ toys-bear-pc/src/components/bsComponents/bsHomeComponent/bsSuperAdminHome.vue
@@ -0,0 +1,26 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+    name:"",
+    data() {
+        return {
+
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
+<style scoped lang='less'>
+
+</style>
