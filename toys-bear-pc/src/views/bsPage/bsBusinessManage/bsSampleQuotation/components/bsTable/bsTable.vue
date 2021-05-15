7a9afa9c3baeffac3c1b92c4f4ae818c0f350c58
jijiliyugou <597892949@qq.com>
2021-05-15 09:25:05 +0800

    完成找样报价详情报个替换

--- toys-bear-pc/src/views/bsPage/bsBusinessManage/bsSampleQuotation/components/bsTable/bsTable.vue
+++ toys-bear-pc/src/views/bsPage/bsBusinessManage/bsSampleQuotation/components/bsTable/bsTable.vue
@@ -33,6 +33,7 @@ export default {
         data: [],
         showLoading: false,
         sizeMini: "mini",
+        isIndex: true,
         columns: [
           {
             prop: "name",
@@ -43,7 +44,7 @@ export default {
             isHiden: true,
             productInfo: true,
             elImage: row => {
-              return row.img;
+              return row.imgUrlList;
             },
             nameHtml: row => {
               return row.name;
@@ -78,12 +79,12 @@ export default {
             }
           },
           {
-            prop: "exhibitionName",
+            prop: "companyName",
             isHiden: true,
             label: "资料来源"
           },
-          { prop: "fa_no", label: "出厂货号", isHiden: true },
-          { prop: "ch_pa", label: "包装", isHiden: true, width: 90 },
+          { prop: "fa_no", width: 60, label: "出厂货号", isHiden: true },
+          { prop: "ch_pa", width: 50, label: "包装", isHiden: true },
           {
             prop: "pr_le",
             label: "产品规格(cm)",
@@ -110,11 +111,11 @@ export default {
           },
           {
             prop: "bulk_stere",
-            label: "体积/材积(cuft)",
+            label: "体积(cbm)/材积(cuft)",
             isHiden: true,
             width: 150,
             render: row => {
-              return row.bulk_stere + "(cbm)/" + row.bulk_feet;
+              return row.bulk_stere + "/" + row.bulk_feet;
             }
           },
           {
@@ -128,25 +129,53 @@ export default {
           {
             prop: "in_en",
             label: "装箱量(pcs)",
-            width: 90,
             isHiden: true,
             render: row => {
               return row.in_en + "/" + row.ou_lo;
             }
           },
           {
+            prop: "boxNumber",
+            label: "箱数",
+            width: 50
+          },
+          {
+            label: "总数量",
+            width: 50,
+            render: row => {
+              return row.boxNumber * row.ou_lo;
+            }
+          },
+          {
             prop: "price",
-            label: "单价",
-            isHiden: true,
-            width: 80,
+            label: "厂价",
+            width: 50,
             color: "red",
             render: row => {
-              return row.cu_de + " " + row.price;
+              return "￥ " + row.price;
+            }
+          },
+          {
+            prop: "offerAmount",
+            label: "报出价",
+            width: 50,
+            color: "red",
+            render: row => {
+              return row.cu_de + " " + row.offerAmount;
+            }
+          },
+          {
+            prop: "OfferTotalAmount",
+            label: "报出总价",
+            width: 60,
+            color: "red",
+            render: row => {
+              return (
+                row.cu_de + " " + row.offerAmount * row.ou_lo * row.boxNumber
+              );
             }
           }
-        ],
-        btnWidth: 100,
-        addIcon: true
+        ]
       }
     };
   },
