9369f371faaa875eaae5bd9d865453ec8d3913df
gaojiahao <1026844853@qq.com>
2021-05-17 14:57:09 +0800

    feat(uni-app初始化)

---
+++ toys-share-uni/pages/offerSharing/offerSharing.vue
@@ -0,0 +1,17 @@
+<template>
+	<view class="baojia">
+		111
+	</view>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="less">
+	.baojia{
+		width: 100%;
+		opacity: 1;
+		background: #5363f6;
+	}
+	
+</style>
