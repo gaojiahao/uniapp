f736cac83d2458afdee991e5d00c8dff7f8a1729
jijiliyugou <597892949@qq.com>
2021-05-25 17:32:14 +0800

    修改配置项

--- toys-bear-pc/src/assets/js/config/imConfig.js
+++ toys-bear-pc/src/assets/js/config/imConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
   devEnv: {
     NODE_ENV: "development", // 本地环境
-    hosturl: "http://139.9.71.135:8089",
-    loginUrl: "http://139.9.71.135:8080/#/me",
-    wsBaseUrl: "ws://139.9.71.135:8080/ws?UserId="
+    hosturl: "http://124.71.6.26:8089",
+    loginUrl: "http://124.71.6.26:8080/#/me",
+    wsBaseUrl: "ws://124.71.6.26:8080/ws?UserId=",
+    appkey: "bmdehs6pbfaas"
     // hosturl: "http://im.toysbear.com",
     // loginUrl: "https://www.toysbear.com/#/me",
     // wsBaseUrl: "wss://impush.toysbear.com/ws?UserId="
@@ -13,9 +14,10 @@ module.exports = {
   },
   testEnv: {
     NODE_ENV: "test", // 测试环境
-    hosturl: "http://139.9.71.135:8089",
-    loginUrl: "http://139.9.71.135:8080/#/me",
-    wsBaseUrl: "ws://139.9.71.135:8080/ws?UserId="
+    hosturl: "http://124.71.6.26:8089",
+    loginUrl: "http://124.71.6.26:8080/#/me",
+    wsBaseUrl: "ws://124.71.6.26:8080/ws?UserId=",
+    appkey: "bmdehs6pbfaas"
     // hosturl: "https://www.toysbear.com",
     // loginUrl: "https://www.toysbear.com/#/me",
     // wsBaseUrl: "wss://impush.toysbear.com/ws?UserId="
@@ -25,7 +27,10 @@ module.exports = {
     // 老测试环境
     hosturl: "http://im.toysbear.com",
     loginUrl: "https://www.toysbear.com/#/me",
-    wsBaseUrl: "wss://impush.toysbear.com/ws?UserId="
+    wsBaseUrl: "wss://impush.toysbear.com/ws?UserId=",
+    // appkey: "pkfcgjstp5888"
+    appkey: "bmdehs6pbfaas"
+
     // 新测试环境
     // hosturl: "http://124.71.6.26:8080",
     // loginUrl: "http://124.71.6.26:8080/#/me",
