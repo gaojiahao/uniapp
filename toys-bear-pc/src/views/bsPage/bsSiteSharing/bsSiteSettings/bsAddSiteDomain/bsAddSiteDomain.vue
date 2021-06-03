47fe3e77b0a482ad31a763960a78fe0d8e93c89f
jijiliyugou <597892949@qq.com>
2021-06-03 20:01:16 +0800

    小竹熊域名

--- toys-bear-pc/src/views/bsPage/bsSiteSharing/bsSiteSettings/bsAddSiteDomain/bsAddSiteDomain.vue
+++ toys-bear-pc/src/views/bsPage/bsSiteSharing/bsSiteSettings/bsAddSiteDomain/bsAddSiteDomain.vue
@@ -28,9 +28,8 @@
       </el-button>
     </center>
     <div class="ipList">
-      <span class="title">小竹熊域名IP：</span>
-      <span class="value">香港IP：119.8.46.84</span>
-      <span class="value">美国IP：132.148.25.167</span>
+      <span class="title">小竹熊域名IP地址：</span>
+      <span class="value">119.29.72.160</span>
     </div>
     <div class="zhinanTitle">
       操作指南
