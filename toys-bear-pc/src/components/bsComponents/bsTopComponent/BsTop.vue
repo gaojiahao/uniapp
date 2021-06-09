05c903d9540c71362657dc87e503c1933b266f32
jijiliyugou <597892949@qq.com>
2021-06-09 15:09:25 +0800

    完成更新版本提示功能

--- toys-bear-pc/src/components/bsComponents/bsTopComponent/BsTop.vue
+++ toys-bear-pc/src/components/bsComponents/bsTopComponent/BsTop.vue
@@ -16,8 +16,8 @@
         </div>
         <div class="isNotice" v-if="vesionList.length">
           <i class="laba iconfont icon-laba1"></i>
-          <p class="noticeContext">
-            热烈庆祝小竹熊签约优选跨界玩具展厅热烈庆祝小竹熊签约优选跨界玩具展厅热烈庆祝小竹熊签约优选跨界玩具展厅
+          <p class="noticeContext" @click="openVesionList">
+            <marqueeLeft :sendVal="vesionList" :duration="1" />
           </p>
         </div>
       </div>
@@ -77,25 +77,62 @@
         </div>
       </div>
     </div>
+    <!-- 更新明细 -->
+    <el-dialog
+      class="detailsDialog"
+      :visible.sync="isShowDetails"
+      width="500px"
+      destroy-on-close
+    >
+      <div class="minxiBox">
+        <div class="minxiBody">
+          <div class="minxiBody_title">
+            <div class="title_label">
+              本次更新内容：
+            </div>
+            <div class="title_date">
+              更新时间：{{
+                vesionList[0] && vesionList[0].createdOn.replace(/T/, " ")
+              }}
+            </div>
+          </div>
+          <div class="minxiBody_body">
+            <el-scrollbar style="height: 100%;">
+              <pre>{{ vesionList[0] && vesionList[0].content }}</pre>
+            </el-scrollbar>
+          </div>
+        </div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 import eventBus from "@/assets/js/common/eventBus";
+import marqueeLeft from "@/components/commonComponent/marquee/marquee.vue";
 export default {
   props: {
+    item: Object,
     isCollapse: {
       type: Boolean
     }
   },
+  components: {
+    marqueeLeft
+  },
   data() {
     return {
+      isShowDetails: false,
       vesionList: [],
       isCheckUser: false
     };
   },
   methods: {
+    // 查看更新明细
+    openVesionList() {
+      this.isShowDetails = true;
+    },
     // 获取版本更新
     async getBearVesionPage() {
       const res = await this.$http.post("/api/BearVesionPage", {
