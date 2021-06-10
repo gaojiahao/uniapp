08d89a578468946e65b0a18c039ae4ff0c26ba60
jijiliyugou <597892949@qq.com>
2021-06-10 09:41:00 +0800

    更新走马灯

--- toys-bear-pc/src/components/commonComponent/marquee/marquee.vue
+++ toys-bear-pc/src/components/commonComponent/marquee/marquee.vue
@@ -3,19 +3,21 @@
     <div class="my-inbox" ref="box">
       <div class="my-list" v-for="(item, index) in sendVal" :key="index">
         <span class="my-uname">{{ item.title }}</span>
-        <span class="my-uname" style="margin-left: 10px;">
-          {{ item.createdOn && item.createdOn.replace(/T/, " ") }}
+        <span class="my-uname" style="margin-left: 10px;" v-if="item.createdOn">
+          {{ item.createdOn.replace(/T/, " ") }}
         </span>
       </div>
-      <div
-        class="my-list"
-        v-for="(item, index) in sendVal"
-        :key="(index + 1) * 100"
-      >
-        <span class="my-uname">{{ item.title }}</span>
-        <span class="my-uname" style="margin-left: 10px;">
-          {{ item.createdOn && item.createdOn.replace(/T/, " ") }}
-        </span>
+      <div class="my-list" v-if="showTwo">
+        <div v-for="(item, index) in sendVal" :key="(index + 1) * 100">
+          <span class="my-uname">{{ item.title }}</span>
+          <span
+            class="my-uname"
+            style="margin-left: 10px;"
+            v-if="item.createdOn"
+          >
+            {{ item.createdOn.replace(/T/, " ") }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -25,6 +27,10 @@ export default {
   name: "my-marquee-left",
   props: {
     sendVal: Array,
+    showTwo: {
+      type: Boolean,
+      default: true
+    },
     duration: {
       type: Number,
       default: 1
